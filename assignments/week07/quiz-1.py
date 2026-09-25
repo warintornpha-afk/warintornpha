e8370dd49d3019d64912731c42f7650097f60b12
Warintorn.pha@ku.th <warintorn.pha@ku.th>
2026-09-25 12:12:59 +0700

    myCircle

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -1,24 +1,31 @@
-"""
-    สร้าง class Rectangle โดยกำหนดให้
-    - มี attribute ชื่อ length และ width ที่เก็บข้อมูลความยาวและความกว้างของสี่เหลี่ยม
-    - มี method ชื่อ get_area() ที่คืนค่าพื้นที่ของสี่เหลี่ยม
-    - มี method ชื่อ get_perimeter() ที่คืนค่ารอบรูปของสี่เหลี่ยม
-"""
-
-class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    # Method to get the area
-    def get_area(self):
-        pass
-
-    # Method to get the perimeter
-    def get_perimeter(self):
-        pass
-
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+print(rect.get_perimeter())  # Should print 30
+
+
+
+
+"""
+ขอให้เขียนคราส circle ที่ทำงานคล้ายคลึงกับคราส Rectangle พร้อมตัวอย่างการทำงาน
+"""
+
+class Circle:
+    # ปรับให้ถูกต้องกับความเป็นวงกลม
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    # Method to get the area ปรับสูตรพื้นที่วงกลม
+    def get_area(self):
+        return self.length * self.width
+
+    # Method to get the perimeter ปรับสูตรเส้นรอบวงวงกลม
+    def get_perimeter(self):
+        return f"Perimeter = {self.length} * {self.width} = {2 * (self.length * self.width)}"
+
+
+myCircle = Circle(10)
+print(myCircle.get_area())
+print(myCircle.get_perimeter())
